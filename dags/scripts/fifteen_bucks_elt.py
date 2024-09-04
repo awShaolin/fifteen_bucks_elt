96b671a80bb43af1a30bf8e44916c741320bec20
work <meshkovmm@gmail.com>
2024-09-04 15:01:09 +0300

    add comments

--- dags/scripts/fifteen_bucks_elt.py
+++ dags/scripts/fifteen_bucks_elt.py
@@ -11,22 +11,26 @@ def fetch_data(api_url):
     return response.json()
 
 def parse_data_to_dataframe(**kwargs):
+    # забирает json из XCom
     data = kwargs['ti'].xcom_pull(task_ids='fetch_data')
     df = pd.DataFrame(data)
     return df
 
 def insert_data_into_dwh(**kwargs):
+    # забирает df из XCom
     df = kwargs['ti'].xcom_pull(task_ids='parse_data_to_df')
-
+    # забирает строку подключения к pg из Aiflow UI
     conn_uri = BaseHook.get_connection('postgres_conn').get_uri()
+    # столкнулся с проблемой, sqlalchemy ругался на формат строки подключения
+    #  тут просто меняю postgres на postgresql в URI
     if conn_uri.startswith("postgres://"):
         conn_uri = conn_uri.replace("postgres://", "postgresql://", 1)
     
     engine = create_engine(conn_uri)
-
+    # механизм подгрузки структуры таблицы из бд
     metadata = MetaData(bind=engine)
     table = Table('cannabis', metadata, schema='raw', autoload_with=engine)
-
+    #забирает id строк, чтобы отвильтровать df и вставить только те, которых тольке нет 
     try:
         with engine.connect() as conn:
             existing_ids_query = conn.execute(f"SELECT id FROM raw.{table.name}")
